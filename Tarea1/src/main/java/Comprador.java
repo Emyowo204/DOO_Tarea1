29d9b087a3327c13534bd1d8d41b40a440b9679e
Chloe Yáñez <agyanez2023@udec.cl>
2024-04-21 02:09:04 -0400

    Actualización de Comprador

--- Tarea1/src/main/java/Comprador.java
+++ Tarea1/src/main/java/Comprador.java
@@ -1,3 +1,32 @@
 package TESTING_1;
-public class Comprador {
+public class Comprador{
+    private String sabor;
+    private int vuelto;
+    public Comprador(Moneda m, int cualBebida, Expendedor exp){
+
+        Producto p = exp.comprarProducto(m,cualBebida);
+
+        if(p!= null){
+            sabor = p.sabor();
+        }else{
+            sabor=null;
+        }
+        Moneda monVuelto=null;
+
+        while (true) {
+            monVuelto = exp.getVuelto();
+            if (monVuelto == null) {
+                break;
+            } else {
+                vuelto += monVuelto.getValor();
+            }
+        }
+
+    }
+    public int cuantoVuelto(){
+        return vuelto;
+    }
+    public String queSabor(){
+        return sabor;
+    }
 }
