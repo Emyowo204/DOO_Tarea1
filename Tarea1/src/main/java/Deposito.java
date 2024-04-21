d9a7732b188bd39ae4480691220e81587d183206
Chloe Yáñez <agyanez2023@udec.cl>
2024-04-21 01:20:50 -0400

    Actualización de Deposito

--- Tarea1/src/main/java/Deposito.java
+++ Tarea1/src/main/java/Deposito.java
@@ -1,2 +1,23 @@
-package TESTING_1;public class Deposito {
+package TESTING_1;
+
+import java.util.ArrayList;
+
+public class Deposito<T>{
+
+    ArrayList<T> contenido = new ArrayList<>();
+
+    public Deposito(){
+
+    }
+    public void addContenido(T t){
+        contenido.add(t);
+    }
+    public T getContenido(){
+        if(contenido.isEmpty()){
+            return null;
+        }else {
+            return contenido.removeFirst();
+        }
+    }
+
 }
