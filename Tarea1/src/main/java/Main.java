cc7f4a3ad5af26f9bfb5d964e236945008daa5d5
Chloe Yáñez <agyanez2023@udec.cl>
2024-04-21 20:28:32 -0400

    Actualización de Main para la prueba de las clases

--- Tarea1/src/main/java/Main.java
+++ Tarea1/src/main/java/Main.java
@@ -1,6 +1,20 @@
 package TESTING_1;
 public class Main {
     public static void main(String[] args) {
-
+        Expendedor exp = new Expendedor(2);
+        Moneda m = null;
+        Comprador c=null;
+        m = new Moneda1000();
+        c = new Comprador(m,Seleccion.CocaCola.getPrecio(),exp);
+        System.out.println(c.queSabor()+", "+c.cuantoVuelto()); // cocacola $700
+        m = new Moneda1000();
+        c = new Comprador(m,Seleccion.CocaCola.getPrecio(),exp);
+        System.out.println(c.queSabor()+", "+c.cuantoVuelto()); // cocacola $700
+        m = new Moneda1000();
+        c = new Comprador(m,Seleccion.CocaCola.getPrecio(),exp);
+        System.out.println(c.queSabor()+", "+c.cuantoVuelto());// cocacola $700
+        m = new Moneda1000();
+        c = new Comprador(m,Seleccion.CocaCola.getPrecio(),exp);
+        System.out.println(c.queSabor()+", "+c.cuantoVuelto()); // cocacola $700
     }
 }
