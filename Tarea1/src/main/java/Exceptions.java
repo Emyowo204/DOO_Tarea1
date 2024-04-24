714d7875fa8c014aa56735883be36ded653a8b9f
Emyowo204 <egaete2023@inf.udec.cl>
2024-04-24 01:31:53 -0400

    Creación de las clases d herederas de RuntimeException; 'NoHayProductoException', 'PagoInsuficienteException' y 'PagoIncorrectoException'

---
+++ Tarea1/src/main/java/Exceptions.java
@@ -0,0 +1,17 @@
+package DOO_Expendedor;
+
+class NoHayProductoException extends RuntimeException {
+    public NoHayProductoException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+class PagoInsuficienteException extends RuntimeException {
+    public PagoInsuficienteException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+class PagoIncorrectoException extends RuntimeException {
+    public PagoIncorrectoException(String errorMessage) {
+        super(errorMessage);
+    }
+}
