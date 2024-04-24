7377c480f584ef3fdb43313ece4320eb4f23a51a
Emyowo204 <egaete2023@inf.udec.cl>
2024-04-24 01:44:43 -0400

    Completación del main del proyecto, junto con try y catchs para las excepciones

--- Tarea1/src/main/java/Main.java
+++ Tarea1/src/main/java/Main.java
@@ -1,3 +1,20 @@
 package DOO_Expendedor;
 public class Main {
+    public static void main(String[] args) {
+        Expendedor exp = new Expendedor(1);
+        Moneda m = null;
+        Comprador c=null;
+
+        try {
+            m = new Moneda100();
+            c = new Comprador(m,1,exp);
+            System.out.println(c.queSabor()+", "+c.cuantoVuelto());
+        } catch (NoHayProductoException e) {
+            System.out.println(e.getMessage());
+        } catch (PagoInsuficienteException e) {
+            System.out.println(e.getMessage());
+        } catch (PagoIncorrectoException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
